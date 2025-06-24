28b29afe0e5f301b412b4b2abd604508b6616062
IamElite <elitemindbusiness@gmail.com>
2025-06-24 10:11:45 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -3,7 +3,7 @@ import re
 import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
-from config import FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, FORCE_SUB_CHANNEL4, ADMINS
+from config import FORCE_SUB_CHANNELS, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 #----Token
@@ -14,7 +14,7 @@ from datetime import datetime
 from database.database import user_data, db_verify_status, db_update_verify_status
 
 async def is_subscribed(filter, client, update):
-    if not (FORCE_SUB_CHANNEL or FORCE_SUB_CHANNEL2 or FORCE_SUB_CHANNEL3 or FORCE_SUB_CHANNEL4):
+    if not FORCE_SUB_CHANNELS:
         return True
 
     user_id = update.from_user.id
@@ -24,10 +24,7 @@ async def is_subscribed(filter, client, update):
 
     member_status = ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER
 
-    for channel_id in [FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, FORCE_SUB_CHANNEL4]:
-        if not channel_id:
-            continue
-
+    for channel_id in FORCE_SUB_CHANNELS:
         try:
             member = await client.get_chat_member(chat_id=channel_id, user_id=user_id)
         except UserNotParticipant:
