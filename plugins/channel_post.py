783c3e1ef8291824fbe36ea27fac7b2451b724a8
IamElite <elitemindbusiness@gmail.com>
2025-06-30 15:24:50 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -12,7 +12,7 @@ from helper_func import *
 from plugins.start import *
 #from plugins.cmd import *
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start', 'users','addshort','resetshort','seeshort', 'getpremiumusers' , 'broadcast', 'batch', 'genlink', 'upi', 'myplan', 'plans', 'stats', 'removepr', 'addpr']))
+@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start', 'users','addshort','resetshort','seeshort', 'getpremiumusers' , 'broadcast', 'batch', 'genlink', 'upi', 'myplan', 'plans', 'stats', 'removepr', 'addpr', 'st']))
 #@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please wait...", quote=True)
