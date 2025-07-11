b67829afc388088679a610b7b15627ac2b7c5901
IamElite <elitemindbusiness@gmail.com>
2025-07-11 17:08:28 +0530

    Update utils.py

--- database/utils.py
+++ database/utils.py
@@ -1,12 +1,6 @@
-from pyrogram import Client
-from pyrogram.enums import ParseMode
-from pyrogram.types import Message
-from datetime import datetime, timedelta
-from zoneinfo import ZoneInfo
+
 import asyncio, time
 from bot import Bot as bot
-from config import *
-from plugins.start import *
 
 
 async def extract_user(bot, message):
@@ -23,28 +17,6 @@ async def extract_user(bot, message):
         except:
             return None
     return None
-
-# logs ------------------->
-
-async def log_action(client: Client, user_id: int, admin: Message, action: str, days: int = 0):
-    user = await client.get_users(user_id)
-    now = datetime.now(ZoneInfo("Asia/Kolkata"))
-    
-    msg = [
-        f"❖ {'🥳' if action == 'Added' else '⚠️'} #{action}Premium ❖\n",
-        f"➜ **ᴀᴄᴛɪᴏɴ:** `{action}`",
-        f"➜ **ᴜsᴇʀ_ɪᴅ:** `{user_id}`",
-        f"➜ **ɴᴀᴍᴇ:** {user.first_name} {user.last_name or ''}".strip(),
-        f"➜ **ᴜsᴇʀɴᴀᴍᴇ:** @{user.username}" if user.username else "➜ **ᴜsᴇʀɴᴀᴍᴇ:** N/A",
-        f"\n➜ **{action.lower()} ʙʏ:** {admin.first_name}",
-        f"➜ **ᴛɪᴍᴇ:** `{now.strftime('%I:%M:%S %p')} (IST)`",
-        f"➜ **ᴅᴀᴛᴇ:** `{now.strftime('%d-%m-%Y')}`"
-    ]
-    
-    if action == "Added":
-        msg.append(f"➜ **ᴇxᴘɪʀᴇs ᴏɴ:** `{(now + timedelta(days=days)).strftime('%d-%m-%Y')}`")
-    
-    await client.send_message(LOG_ID, "\n".join(msg), parse_mode=ParseMode.MARKDOWN)
     
 
 async def send_premium_reminders(bot_instance, phdlust):
