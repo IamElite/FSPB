b764453e79fbbc3381dd13f8444cd2c3cacd5ee8
IamElite <elitemindbusiness@gmail.com>
2025-07-15 13:51:27 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -253,7 +253,7 @@ async def start_command(client: Client, message):
                         short_link = linkb
                 
                         for _ in range(count):
-                            short_link = await get_shortlink(SHORT_URL, SHORT_API, short_link)
+                            short_link = await get_shortlink(SHORT_URL, short_link)
                 
                     except Exception as e:
                         error_msg = str(e)
